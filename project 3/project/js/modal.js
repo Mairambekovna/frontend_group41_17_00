d1d34e4ac528dca1674bd990ba2d00ec967b92a9
Mairambekovna <azaliyamairambekovna@gmail.com>
2024-09-12 12:36:10 +0600

    add  3 homework

---
+++ project 3/project/js/modal.js
@@ -0,0 +1,39 @@
+// MODAL
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('#btn-get')
+const modalCloseButton = document.querySelector('.modal_close')
+const openModal = () => {
+    modal.style.display = 'block'
+    document.body.style.overflow = 'hidden'
+}
+
+const closeModal = () => {
+    modal.style.display = 'none'
+    document.body.style.overflow = ''
+}
+modalTrigger.onclick = () => openModal ()
+modalCloseButton.onclick = () => closeModal()
+modal.onclick = (event) => {
+    if (event.target === modal)
+    closeModal()
+}
+
+
+let switchModal = true
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY + window.innerHeight
+    const scrollHeight =document.body.scrollHeight
+    if (scrollPosition >= scrollHeight && switchModal === true){
+        openModal()
+        switchModal = false
+    }
+    window.removeEventListener('scroll', openModal)
+
+})
+
+setTimeout(() => {
+    openModal()},10000)
+
+
+
